cce495f19b219ac8378eb590fe9158ab58cebcaa
InHall Technologies <inhall.updatebackup@gmail.com>
2022-04-13 12:54:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [userInput, setSomething] = useState({
+    name: "",
+    usn:"",
+    university:"",
+    gender:"",
+    phoneNumber:''
+  });
+  const [shouldIShowValue, setShouldIShowValue] = useState(false);
+  
+  // i am a valid statement? I will be exicuted: else i will be exicuted
+
+
+  const handleNameChange = event => {
+    const newObject = {
+     ...userInput,
+     name: event.target.value 
+    }
+     
+    setSomething(newObject)
+  }
+
+  const handleDataVisibility = () => {
+    setShouldIShowValue(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input 
+        placeholder='Name' 
+        onChange={handleNameChange} 
+        value={userInput.name} />
+
+        {
+          shouldIShowValue
+          ?
+          <h1>{userInput.name}</h1>
+          :
+          null
+        }
+
+        <button onClick={handleDataVisibility} >Show Name</button>
     </div>
   );
 }
